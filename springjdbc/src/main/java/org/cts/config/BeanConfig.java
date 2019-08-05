8b0f4a5cd1e02857c6456d16e690bc9bea91f4e6
Aditya Chakraborty <bhutai007@gmail.com>
2019-08-05 18:36:38 +0530

    ssaa

--- springjdbc/src/main/java/org/cts/config/BeanConfig.java
+++ springjdbc/src/main/java/org/cts/config/BeanConfig.java
@@ -9,12 +9,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.cts")
 @PropertySource("classpath:db.properties")
+@EnableTransactionManagement
 public class BeanConfig {
 
     @Value("${mysql.driverName}")
